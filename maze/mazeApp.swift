8f926418ab1afabd1ce02302bb996c7d8b1fd52d
diuta <dimasptra@gmail.com>
2026-03-19 12:57:18 +0700

    Initial Commit

---
+++ maze/mazeApp.swift
@@ -0,0 +1,17 @@
+//
+//  mazeApp.swift
+//  maze
+//
+//  Created by Dimas Putra Aryawan on 19/03/26.
+//
+
+import SwiftUI
+
+@main
+struct mazeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
